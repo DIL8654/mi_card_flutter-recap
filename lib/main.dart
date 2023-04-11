2b6c5d765e228d28f02629326ec5b341136305ba
Dilanka Muthukumarana <dilanka85@gmail.com>
2023-04-11 11:38:54 +0200

    simple flutter app
    simple stateful app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int clicks = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Demo app'),
+        ),
+        body: Center(
+          child: Text('Number of clicks:  $clicks'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              clicks++;
+            });
+          },
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
