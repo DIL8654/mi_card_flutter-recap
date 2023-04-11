f76dcf16057213e82ef6a1361248e37484de8b77
Dilanka Muthukumarana <dilanka85@gmail.com>
2023-04-11 14:25:26 +0200

    contact card initial

--- lib/main.dart
+++ lib/main.dart
@@ -11,43 +11,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                width: 100,
-                color: Colors.red,
-                child: Text('Hello'),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.teal,
+              backgroundImage: AssetImage('images/dilanka.jpg'),
+            ),
+            Text(
+              'Dilanka Muthukumarana',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
               ),
-              Container(
-                width: 230,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.yellow,
-                      child: Text('Hello'),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                      child: Text('Hello'),
-                    )
-                  ],
-                ),
+            ),
+            Text(
+              'Staff Software Engineer',
+              style: TextStyle(
+                color: Colors.teal.shade100,
+                fontFamily: 'Exo2',
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
               ),
-              Container(
-                width: 100,
-                color: Colors.blue,
-                child: Text('Hello'),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+47 123456789',
+                    style:
+                        TextStyle(color: Colors.teal.shade900, fontSize: 30.0),
+                  )
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'dilanka@gmail.com',
+                    style:
+                        TextStyle(color: Colors.teal.shade900, fontSize: 30.0),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
